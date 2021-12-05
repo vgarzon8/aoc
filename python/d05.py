637b2cf3b61b8ef009cc08ce9f0541afcfc74579
Victor Garzon <victor.garzon@bain.com>
2021-12-05 06:56:15 -0800

    add grid class

--- python/d05.py
+++ python/d05.py
@@ -3,71 +3,37 @@
 # https://adventofcode.com/2021/day/5
 # Hydrothermal venture, map with overlapping lines
 
+class Map:
+    def __init__(self, dat):
+        self.dat = dat
+        self.xmax = max([max([p[0][0], p[1][0]]) for p in dat]) + 1
+        self.ymax = max([max([p[0][1], p[1][1]]) for p in dat]) + 1
+        self.grid = {
+            (i, j): 0 for j in range(self.ymax) for i in range(self.xmax)
+        }
 
-def part_1(input_file):
-    dat = prep_data(input_file)
+    def update_up_down(self):
+        for k, (beg, end) in enumerate(self.dat):
+            if beg[0] == end[0]:
+                if beg[1] < end[1]:
+                    r = range(beg[1], end[1] + 1)
+                else:
+                    r = range(end[1], beg[1] + 1)
+                for j in r:
+                    self.grid[(beg[0], j)] += 1
 
-    xmax = max([max([p[0][0], p[1][0]]) for p in dat]) + 1
-    ymax = max([max([p[0][1], p[1][1]]) for p in dat]) + 1
+            elif beg[1] == end[1]:
+                if beg[0] < end[0]:
+                    r = range(beg[0], end[0] + 1)
+                else:
+                    r = range(end[0], beg[0] + 1)
+                for i in r:
+                    self.grid[(i, beg[1])] += 1
 
-    print(xmax, ymax)
-
-    grid = {(i, j): 0 for j in range(ymax) for i in range(xmax)}
-
-    for k, (beg, end) in enumerate(dat):
-        if beg[0] == end[0]:
-            if beg[1] < end[1]:
-                r = range(beg[1], end[1] + 1)
-            else:
-                r = range(end[1], beg[1] + 1)
-            for j in r:
-                grid[(beg[0], j)] += 1
-
-        elif beg[1] == end[1]:
-            if beg[0] < end[0]:
-                r = range(beg[0], end[0] + 1)
-            else:
-                r = range(end[0], beg[0] + 1)
-            for i in r:
-                grid[(i, beg[1])] += 1
-
-    # for j in range(ymax):
-    #     print("".join([str(grid[(i, j)]) for i in range(xmax)]))
-
-    count = sum([
-        1 if grid[(i, j)] > 1 else 0
-        for j in range(ymax) for i in range(xmax)
-    ])
-
-    return count
-
-
-def part_2(input_file):
-    dat = prep_data(input_file)
-
-    xmax = max([max([p[0][0], p[1][0]]) for p in dat]) + 1
-    ymax = max([max([p[0][1], p[1][1]]) for p in dat]) + 1
-
-    grid = {(i, j): 0 for j in range(ymax) for i in range(xmax)}
-
-    for k, (beg, end) in enumerate(dat):
-        if beg[0] == end[0]:
-            if beg[1] < end[1]:
-                r = range(beg[1], end[1] + 1)
-            else:
-                r = range(end[1], beg[1] + 1)
-            for j in r:
-                grid[(beg[0], j)] += 1
-
-        elif beg[1] == end[1]:
-            if beg[0] < end[0]:
-                r = range(beg[0], end[0] + 1)
-            else:
-                r = range(end[0], beg[0] + 1)
-            for i in r:
-                grid[(i, beg[1])] += 1
-
-        else:
+    def update_diag(self):
+        for k, (beg, end) in enumerate(self.dat):
+            if beg[0] == end[0] or beg[1] == end[1]:
+                continue
             length = abs(end[0] - beg[0]) + 1
             for k in range(length):
                 if beg[0] < end[0]:
@@ -76,17 +42,37 @@ def part_2(input_file):
                     x = end[0] + k
                 m = (end[1] - beg[1]) / (end[0] - beg[0])
                 y = m * (x - beg[0]) + beg[1]
-                grid[(x, y)] += 1
+                self.grid[(x, y)] += 1
 
-    # for j in range(ymax):
-    #     print("".join([str(grid[(i, j)]) for i in range(xmax)]))
+    def count_overlaps(self):
+        return sum([
+            1 if self.grid[(i, j)] > 1 else 0
+            for j in range(self.ymax) for i in range(self.xmax)
+        ])
 
-    count = sum([
-        1 if grid[(i, j)] > 1 else 0
-        for j in range(ymax) for i in range(xmax)
-    ])
+    def print_state(self):
+        for j in range(self.ymax):
+            print("".join([str(self.grid[(i, j)]) for i in range(self.xmax)]))
 
-    return count
+
+def part_1(input_file, debug=False):
+    dat = prep_data(input_file)
+    g = Map(dat)
+    g.update_up_down()
+    if debug:
+        g.print_state()
+    return g.count_overlaps()
+
+
+def part_2(input_file, debug=False):
+    dat = prep_data(input_file)
+    g = Map(dat)
+    g.update_up_down()
+    g.update_diag()
+    if debug:
+        g.print_state()
+
+    return g.count_overlaps()
 
 
 def read_lines(input_file):
@@ -108,10 +94,8 @@ if __name__ == "__main__":
     test_input_a = "data/05a.txt"
     input_file = "data/05.txt"
 
-    # part 1
-    assert part_1(test_input_a) == 5
+    assert part_1(test_input_a, debug=True) == 5
     assert part_1(input_file) == 5632
 
-    # part 2
-    assert part_2(test_input_a) == 12
+    assert part_2(test_input_a, debug=True) == 12
     assert part_2(input_file) == 22213
