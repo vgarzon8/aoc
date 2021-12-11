d8f35cc3e6ed4dcf72962d31d815dc32d962ed16
Victor Garzon <victor.garzon@bain.com>
2021-12-10 16:12:53 -0800

    d10 rust v1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,9 +3,10 @@
 mod util;
 // mod d01;
 // mod d02;
-mod d05;
+// mod d05;
 // mod d06;
 // mod d07;
+mod d10;
 
 fn main() {
     // // day 1
@@ -14,11 +15,11 @@ fn main() {
     // // day 2
     // util::print_ans(d02::part1("../data/02.txt"));  // ans: 1924923
     // util::print_ans(d02::part2("../data/02.txt"));  // ans: 1982495697
-    // day 5
-    assert_eq!(d05::part1("../data/05a.txt", true).unwrap(), 5);
-    assert_eq!(d05::part1("../data/05.txt", false).unwrap(), 5632);
-    assert_eq!(d05::part2("../data/05a.txt", true).unwrap(), 12);
-    assert_eq!(d05::part2("../data/05.txt", false).unwrap(), 22213);
+    // // day 5
+    // assert_eq!(d05::part1("../data/05a.txt", true).unwrap(), 5);
+    // assert_eq!(d05::part1("../data/05.txt", false).unwrap(), 5632);
+    // assert_eq!(d05::part2("../data/05a.txt", true).unwrap(), 12);
+    // assert_eq!(d05::part2("../data/05.txt", false).unwrap(), 22213);
     // // day 6
     // assert_eq!(d06::count_dict("../data/06a.txt", 80).unwrap(), 5934);
     // assert_eq!(d06::count_dict("../data/06.txt", 80).unwrap(), 386640);
@@ -29,4 +30,7 @@ fn main() {
     // assert_eq!(d07::part1("../data/07.txt").unwrap(), 337833);
     // assert_eq!(d07::part2("../data/07a.txt").unwrap(), 168);
     // assert_eq!(d07::part2("../data/07.txt").unwrap(), 96678050);
+    // day 10
+    assert_eq!(d10::syntax_score("../data/10a.txt").unwrap(), (26397, 288957));
+    assert_eq!(d10::syntax_score("../data/10.txt").unwrap(), (344193, 3241238967));
 }
