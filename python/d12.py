049312d4af20bc100a2e62c79a802c7b8d7b8812
Victor Garzon <victor.garzon@bain.com>
2021-12-14 20:47:49 -0800

    d12 python recursive

--- python/d12.py
+++ python/d12.py
@@ -3,92 +3,36 @@
 # https://adventofcode.com/2021/day/12
 # Passage paths
 
+# Source: https://github.com/AxlLind/AdventOfCode2021/blob/main/src/bin/12.rs
+
 import networkx as nx
 
 
-class Graph:
-    def __init__(self, dat):
-        self.g = nx.Graph(dat)
-        self.paths = []
+def count_util(graph, src, path, seen_twice):
+    if src == "end":
+        return 1
 
-    def all_paths_part1(self, current, end, visited, path):
-        visited[current] = True
-        path.append(current)
+    if src.islower() and src in path:
+        if seen_twice or src == "start":
+            return 0
 
-        if current == end:
-            self.paths.append(path.copy())
-        else:
-            for i in self.g[current]:
-                if visited[i] is False or i.isupper():
-                    self.all_paths_part1(i, end, visited, path)
+        seen_twice = True
 
-        path.pop()
-        visited[current] = False
+    path.append(src)
+    ans = sum([count_util(graph, i, path, seen_twice) for i in graph[src]])
+    path.pop()
 
-    def all_paths_part2(self, current, end, visited, path, count):
-        visited[current] = True
-        count[current] += 1
-        path.append(current)
-
-        if current == end:
-            if path not in self.paths:
-                self.paths.append(path.copy())
-        else:
-            for i in self.g[current]:
-                cnt = [
-                    c for (k, c) in count.items()
-                    if (k.islower() and k != "start" and k != "end")
-                ]
-                mask = (
-                    visited[i] is False or i.isupper() is True
-                    or (
-                        i != "start"
-                        and i.islower()
-                        and all([c <= 2 for c in cnt])
-                        and (len([c for c in cnt if c > 1]) < 2)
-                    )
-                )
-                if mask:
-                    self.all_paths_part2(i, end, visited, path, count)
- 
-        path.pop()
-        visited[current] = False
-        count[current] -= 1
-
-    def all_paths(self, start, end, mode="part1"):
-        visited = {}
-        for k in self.g.nodes:
-            visited[k] = False
-
-        count = {}
-        for k in self.g.nodes:
-            count[k] = 0
-
-        path = []
-        if mode == "part1":
-            self.all_paths_part1(start, end, visited, path)
-        elif mode == "part2":
-            self.all_paths_part2(start, end, visited, path, count)
-        else:
-            raise Exception(
-                f"mode should be one of 'part1' or 'part'2, got {mode}"
-            )
-
-        return self.paths
+    return ans
 
 
-def count_paths(input_file, mode="part1", verbose=False):
-    caves = Graph(prep_data(input_file))
-    paths = caves.all_paths("start", "end", mode=mode)
+def count_paths(input_file, mode="part1"):
+    graph = nx.Graph(prep_data(input_file))
+    print(graph)
 
-    if verbose is True:
-        print(caves.g)
-        print("nodes", caves.g.nodes)
-        print("edges", caves.g.edges)
-        for p in paths:
-            print(p)
+    seen_twice = True if mode == "part1" else False
+    count = count_util(graph, "start", [], seen_twice)
 
-    return len(paths)
+    return count
 
 
 def read_lines(input_file):
@@ -106,13 +50,12 @@ def prep_data(input_file):
 if __name__ == "__main__":
 
     valid_file = "data/12a.txt"
-    # valid_file = "data/12b.txt"
-    # valid_file = "data/12c.txt"
     input_file = "data/12.txt"
 
-    # # part 1
-    # assert count_paths(valid_file) == 10
-    # assert count_paths(input_file) == 3450
+    # part 1
+    assert count_paths(valid_file) == 10
+    assert count_paths(input_file) == 3450
 
-    # # part 2
-    print(count_paths(valid_file, "part2", True))
+    # part 2
+    assert count_paths(valid_file, "part2") == 36
+    assert count_paths(input_file, "part2") == 96528
